02da9434c5349fb9abcf1bc1f04b677c4dc78443
Zach Rosario <zrosario301@gmail.com>
2026-01-27 15:11:12 -0500

    handle weird edge case after updating to postgres 18

--- tables/mv_books_dc.sql
+++ tables/mv_books_dc.sql
@@ -12,9 +12,12 @@ BEGIN
     CREATE EXTENSION IF NOT EXISTS pg_trgm;
 EXCEPTION 
     WHEN duplicate_function THEN
-        CREATE EXTENSION pg_trgm FROM unpackaged;
+        BEGIN
+            CREATE EXTENSION pg_trgm FROM unpackaged;
+        EXCEPTION WHEN OTHERS THEN
+            NULL;
+        END;
 END $$;
-ALTER EXTENSION pg_trgm UPDATE;
 
 DROP MATERIALIZED VIEW IF EXISTS mv_books_dc CASCADE;
 CREATE MATERIALIZED VIEW mv_books_dc AS
